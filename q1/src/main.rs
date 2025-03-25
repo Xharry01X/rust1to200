c9dbf2d5125e309263d2822a4e77337e189b7f8c
Xharry01X <harshitsingh50621@gmail.com>
2025-03-26 02:23:15 +0530

    swaping number..

--- q1/src/main.rs
+++ q1/src/main.rs
@@ -1,16 +1,16 @@
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
 
 
-fn sum(a: i32, b: i32) -> i32 {
-    add(a, b) 
-}
 
 fn main() {
-    let num1 = 10;
-    let num2 = 20;
-    let result = sum(num1, num2);  
-    println!("The sum of {} and {} is: {}", num1, num2, result);
+    let mut a = 2;
+    let mut b = 4;
+
+    let temp = a;
+    a = b;
+    b = temp;
+
+    println!("After swap the value a: a = {}, b={}",a,b);
+
+
 }
