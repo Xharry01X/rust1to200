04465b43f5e7d803a8a703baa9df647e9ab3f292
Xharry01X <harshitsingh50621@gmail.com>
2025-03-27 09:48:35 +0530

    understanding mutable and immutable

--- q3/main.rs
+++ q3/main.rs
@@ -1,9 +1,20 @@
+
+
+// you can have multiple immutable vaules but one mutable value and no mixing allowed
+
 fn main() {
-    let s = String::from("hello");
-    let len = calculate_length(&s); 
-    println!("Length of '{}' is {}.", s, len);
+    let mut s = String::from("hello");
+
+    let r1 = &s;
+    let r2 = &s;
+
+    println!("{} and {}",r1,r2);
+
+    let r3 = &mut s; // it's a mutable refernce 
+
+r3.push_str(", world");
+println!("{}",r3);
 }
 
-fn calculate_length(s: &String) -> usize { 
-    s.len()
-} 
+
+
