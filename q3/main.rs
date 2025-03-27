bed9c213d24f7649b5f715384a94c7055ca8879f
Xharry01X <harshitsingh50621@gmail.com>
2025-03-27 10:31:57 +0530

    mutable referenece.

--- q3/main.rs
+++ q3/main.rs
@@ -3,17 +3,15 @@
 // you can have multiple immutable vaules but one mutable value and no mixing allowed
 
 fn main() {
-    let mut s = String::from("hello");
+    let mut x = 5;
 
-    let r1 = &s;
-    let r2 = &s;
+    let y = &x;
 
-    println!("{} and {}",r1,r2);
+    println!("{} value of x and value of Y {}",x,y);
 
-    let r3 = &mut s; // it's a mutable refernce 
-
-r3.push_str(", world");
-println!("{}",r3);
+    let z = &mut x;
+    *z += 1;
+    println!("Value of z is {}",z);
 }
 
 
